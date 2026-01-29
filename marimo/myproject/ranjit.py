c00c10fa4c6bee90347ab8bdeee7c54d388291f3
Ranjit Singh <singhranjeet45757@gmail.com>
2026-01-29 20:57:18 +0100

    done

--- marimo/myproject/ranjit.py
+++ marimo/myproject/ranjit.py
@@ -14,12 +14,60 @@ def _():
     import matplotlib.pyplot as plt
     from sklearn.cluster import KMeans
     from sklearn.preprocessing import StandardScaler
-    return (pd,)
+    return pd, plt, sns
 
 
 @app.cell
 def _(pd):
-    df = pd.read_csv("../marimo/myproject/happy.csv")
+    df = pd.read_csv("../myproject/happy.csv")
+    return (df,)
+
+
+@app.cell
+def _(df):
+    # Show the first rows
+    df.head()
+
+
+    return
+
+
+@app.cell
+def _():
+    import marimo as mo
+
+    top_n = mo.ui.slider(5, 30, value=10, label="Select number of top countries")
+    top_n
+
+    return (top_n,)
+
+
+@app.cell
+def _(df, top_n):
+    n = top_n.value
+
+    top_countries = (
+        df[['Country or region', 'Score']]
+        .sort_values(by='Score', ascending=False)
+        .head(n)
+    )
+
+    top_countries
+
+    return n, top_countries
+
+
+@app.cell
+def _(n, plt, sns, top_countries):
+
+    plt.figure(figsize=(10,6))
+    sns.barplot(data=top_countries, x='Score', y='Country or region')
+    plt.title(f'Top {n} Happiest Countries')
+    plt.xlabel('Happiness Score')
+    plt.ylabel('Country or region')
+    plt.tight_layout()
+    plt.show()
+
     return
 
 
